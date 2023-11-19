a189090b2a8418482981ba0bfde8de87d618c190
Ganner86 <144483695+Ganner86@users.noreply.github.com>
2023-11-19 22:04:55 +0100

    Create domesticwater.js

---
+++ build/src/lib/domesticwater.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const component_1 = __importDefault(require("iobroker.hdg-bavaria/build/src/lib/component"));
+class domesticwater extends component_1.default {
+}
+exports.default = domesticwater;
+//# sourceMappingURL=domesticwater.js.map
